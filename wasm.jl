e5407a33e43c33f4ee443560da2506e886af6674
WooKyoung Noh <wookay.noh@gmail.com>
2018-03-29 22:20:23 +0900

    Router.call

--- wasm.jl
+++ wasm.jl
@@ -141,7 +141,7 @@ import Sockets: @ip_str
 Bukdu.start(parse(Int,ENV["PORT"]); host=ip"0.0.0.0")
 # Bukdu.start(8080)
 
-Router.request(get, "/") #
+Router.call(get, "/") #
 # CLI.routes()
 
 Base.JLOptions().isinteractive==0 && wait()
